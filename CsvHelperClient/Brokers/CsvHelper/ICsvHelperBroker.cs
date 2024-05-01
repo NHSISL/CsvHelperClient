a20c95e3b522a0cd84320a1cacf6650368c3c4e4
Christo du Toit <christo@dutoit.co.uk>
2024-05-01 12:49:40 +0100

    BROKERS: CSV Reader / Writer

---
+++ CsvHelperClient/Brokers/CsvHelper/ICsvHelperBroker.cs
@@ -0,0 +1,16 @@
+﻿// ---------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------
+
+using System.IO;
+using CsvHelper;
+
+namespace CsvHelperClient.Brokers.CsvHelper
+{
+    public interface ICsvHelperBroker
+    {
+        CsvReader CreateCsvReader(StringReader reader, bool hasHeaderRecord);
+        CsvWriter CreateCsvWriter(StringWriter writer, bool hasHeaderRecord);
+        StringWriter CreateStringWriter();
+    }
+}
