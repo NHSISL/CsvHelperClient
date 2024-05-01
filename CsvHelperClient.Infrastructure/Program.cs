c1d992956434cd277edd4161932ebe3303c8f28d
Christo du Toit <christo@dutoit.co.uk>
2024-05-01 12:38:55 +0100

    INFRA: Initial checkin

---
+++ CsvHelperClient.Infrastructure/Program.cs
@@ -0,0 +1,17 @@
+﻿// ---------------------------------------------------------------
+// Copyright (c) North East London ICB. All rights reserved.
+// ---------------------------------------------------------------
+
+using CsvHelperClient.Infrastructure.Services;
+
+namespace CsvHelperClient.Infrastructure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var scriptGenerationService = new ScriptGenerationService();
+            scriptGenerationService.GenerateBuildScript();
+        }
+    }
+}
