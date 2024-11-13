e846afd46c47e0be355bdafc80c624b4fc08b5e8
MichaelOSullivanAnswer <michael.osullivan@answerdigital.com>
2024-11-13 11:44:04 +0000

    ShouldThrowValidationExceptionOnMapCsvToObjectIfPlainObjectAndTrailingCommaInvalidAndLogItAsync -> FAIL

---
+++ NHSISL.CsvHelperClient/Models/Foundations/CsvHelpers/Exceptions/InvalidCsvHelperArgumentCombinationException.cs
@@ -0,0 +1,11 @@
+﻿using Xeptions;
+
+namespace NHSISL.CsvHelperClient.Models.Foundations.CsvHelpers.Exceptions
+{
+    public class InvalidCsvHelperArgumentCombinationException : Xeption
+    {
+        public InvalidCsvHelperArgumentCombinationException(string message)
+            : base(message)
+        { }
+    }
+}
